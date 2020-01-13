04663eee168f895ede76a70bb097a64010b29da5
hulei <hulei@deepexi.com>
2020-01-13 19:43:07 +0800

    feat: 购物车页面添加收货地址

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,66 +1,31 @@
-// pages/cart/index.js
+import regeneratorRuntime from '../../lib/runtime/runtime'
+import{getSetting,chooseAddress,openSetting} from '../../utils/asyncWeChat.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    address:{}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onShow(){
+    const address = wx.getStorageSync('address');
+    this.setData({
+      address
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+   async handleGetAddress(){
+    try {
+      let res1 = await getSetting()
+      const scopeAddress = res1.authSetting["scope.address"]
+      if(scopeAddress === false){
+        await openSetting()
+      }
+      let address = await chooseAddress()
+      const {provinceName,cityName,countyName,detailInfo} = address
+      address.all = provinceName + cityName + countyName + detailInfo
+      //将获取到的地址信息存到storage
+      wx.setStorageSync('address', address)
+    } catch (error) {}
   }
 })
