e6a83094f84dbe8cfc6e9300f85ed052eb24c753
hulei <hulei@deepexi.com>
2020-01-13 14:07:01 +0800

    feat: 完善商品详情页面

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,66 +1,43 @@
-// pages/goods_detail/index.js
+const api = require('../../utils/api')
+import {request} from '../../request/index'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goodsInfo:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const {goods_id} = options
+    this.getGoodsInfo(goods_id)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  getGoodsInfo(id){
+    request({
+      url: api.goodsDetail,
+      data: {goods_id:id}
+    }).then(({data})=>{
+      const {goods_name,goods_introduce,pics,goods_price} = data.message
+      this.setData({
+        goodsInfo:{
+          goods_name,
+          pics,
+          goods_price,
+          goods_introduce:goods_introduce.replace(/\.webp/g,'.jpg')
+        }
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  previewImg(e){
+    const {src,urls} = e.currentTarget.dataset
+    const urlArr = urls.map(it=>it.pics_mid)
+    wx.previewImage({
+      urls:urlArr,
+      current:src
+    })
   }
 })
